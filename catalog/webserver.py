f1b1d7b88296458da360500a67845df28c95d2cc
Corbin Buckley <corbinbuckley@gmail.com>
2017-08-23 12:27:59 -0600

    add Objective 4
    Working

--- catalog/webserver.py
+++ catalog/webserver.py
@@ -101,6 +101,25 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurant')
+                    restaurantIDPath = self.path.split("/")[2]
+
+                    restaurantEdit = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if restaurantEdit != []:
+                        restaurantEdit.name = messagecontent[0]
+                        session.add(restaurantEdit)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurant')
+                        self.end_headers()
+
             if self.path.endswith("/restaurant/new"):
 
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
