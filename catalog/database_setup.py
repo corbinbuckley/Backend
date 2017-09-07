af3ea24aeeb66ea967eec3cfe416be701bbaac07
Corbin Buckley <corbinbuckley@gmail.com>
2017-09-07 08:37:19 -0600

    Add user to data base

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -10,11 +10,29 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -25,8 +43,9 @@ class MenuItem(Base):
     description = Column(String(250))
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 # We added this serialize function to be able to send JSON objects in a
 # serializable format
@@ -43,6 +62,7 @@ class MenuItem(Base):
 
 ##### insert at end of file ######
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+# engine = create_engine('sqlite:///restaurantmenu.db')
 
 Base.metadata.create_all(engine)
